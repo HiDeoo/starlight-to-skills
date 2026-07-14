c7cd66cede5ee72cec4632ada04007b73f4c003d
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-07-14 15:13:57 +0200

    chore: bootstrap project

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# starlight-skills-sync
+<div align="center">
+  <h1>starlight-skills-sync 🚧</h1>
+  <p>starlight-skills-sync description</p>
+  <p>
+    <a href="https://dummyimage.com/520x350/121212/cdc8be.png&text=screenshot" title="Screenshot of starlight-skills-sync">
+      <img alt="Screenshot of starlight-skills-sync" src="https://dummyimage.com/520x350/121212/cdc8be.png&text=screenshot" width="520" />
+    </a>
+  </p>
+</div>
+
+<div align="center">
+  <a href="https://github.com/HiDeoo/starlight-skills-sync/actions/workflows/integration.yml">
+    <img alt="Integration Status" src="https://github.com/HiDeoo/starlight-skills-sync/actions/workflows/integration.yml/badge.svg" />
+  </a>
+  <a href="https://github.com/HiDeoo/starlight-skills-sync/blob/main/LICENSE">
+    <img alt="License" src="https://badgen.net/github/license/HiDeoo/starlight-skills-sync" />
+  </a>
+  <br />
+</div>
+
+## Features
+
+starlight-skills-sync presentation.
+
+- starlight-skills-sync feature 1
+- starlight-skills-sync feature 2
+
+## License
+
+Licensed under the MIT License, Copyright © HiDeoo.
+
+See [LICENSE](https://github.com/HiDeoo/starlight-skills-sync/blob/main/LICENSE) for more information.
---
+++ docs/astro.config.ts
@@ -0,0 +1,68 @@
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
+
+const site =
+  (process.env['CONTEXT'] === 'production' ? process.env['URL'] : process.env['DEPLOY_PRIME_URL']) ??
+  'https://starlight-skills-sync.netlify.app/'
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      description: '// TODO(HiDeoo) ',
+      editLink: {
+        baseUrl: 'https://github.com/HiDeoo/starlight-skills-sync/edit/main/docs/',
+      },
+      head: [
+        {
+          tag: 'meta',
+          attrs: {
+            property: 'og:image',
+            content: new URL('og.jpg', site).href,
+          },
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            property: 'og:image:alt',
+            content: '// TODO(HiDeoo) ',
+          },
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Start Here',
+          // TODO(HiDeoo)
+          items: [],
+          // items: ["getting-started", "configuration"],
+        },
+        {
+          label: 'Guides',
+          // TODO(HiDeoo)
+          items: [],
+          // items: ["guides/custom-highlights"],
+        },
+        {
+          label: 'Resources',
+          // TODO(HiDeoo)
+          items: [],
+          // items: [{ label: "Plugins and Tools", slug: "resources/starlight" }],
+        },
+      ],
+      social: [
+        {
+          href: 'https://bsky.app/profile/hideoo.dev',
+          icon: 'blueSky',
+          label: 'Bluesky',
+        },
+        {
+          href: 'https://github.com/HiDeoo/starlight-skills-sync',
+          icon: 'github',
+          label: 'GitHub',
+        },
+      ],
+      title: 'Starlight Skills Sync',
+    }),
+  ],
+  site,
+  trailingSlash: 'always',
+})
